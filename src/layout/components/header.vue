a22d2cba5293251392f73a0523f4fd85640879d3
Monkey-qiang <15172750633@163.com>
2023-02-10 09:38:26 +0800

    header顶部栏修改

--- src/layout/components/header.vue
+++ src/layout/components/header.vue
@@ -8,28 +8,28 @@
             <div class="flex align-center text-center">
               <div class="m-r-14 l-h-24 b-r-4 w-48 h-24 cursor-point bg-FFC304 ff-Medium text-c070707 font-14 fw-500" @click="login">登录</div>
               <div class="m-r-20 l-h-24 b-r-4 w-48 h-24 cursor-point bg-EDEDED ff-Medium text-c070707 font-14 fw-500" @click="apply">申请</div>
-            </div>
-            <div class="flex align-center">
-                <el-dropdown class="text-white cursor-point m-r-24" @command="handleCommand">
-                    <span class="el-dropdown-link">
-                        {{ language }}<i class="el-icon-arrow-down el-icon--right"></i>
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item class="flex align-center justify-between text-c001529 p-l-32 p-r-24" command="zh">
-                            简体中文
-                            <img v-if="language == '简体中文'" src="../../assets/common/selected.png" alt="">
-                        </el-dropdown-item>
-                        <el-dropdown-item class="flex align-center justify-between text-c001529 p-l-32 p-r-24" command="en">
-                            English
-                            <img v-if="language == 'English'" src="../../assets/common/selected.png" alt="">
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-                <div class="line w-1 h-20 bg-c96A0B5 m-r-24" v-if="isLogin"></div>
-                <div class="flex align-center cursor-point" @click="loginOut" v-if="isLogin">
-                    <img src="../../assets/common/exit.png" alt="">
-                    <div class="text-white m-l-4 m-r-24">{{ $t('common.exit') }}</div>
-                </div>
+              <div class="flex align-center">
+                  <el-dropdown class="text-white cursor-point m-r-24" @command="handleCommand">
+                      <span class="el-dropdown-link">
+                          {{ language }}<i class="el-icon-arrow-down el-icon--right"></i>
+                      </span>
+                      <el-dropdown-menu slot="dropdown">
+                          <el-dropdown-item class="flex align-center justify-between text-c001529 p-l-32 p-r-24" command="zh">
+                              简体中文
+                              <img v-if="language == '简体中文'" src="../../assets/common/selected.png" alt="">
+                          </el-dropdown-item>
+                          <el-dropdown-item class="flex align-center justify-between text-c001529 p-l-32 p-r-24" command="en">
+                              English
+                              <img v-if="language == 'English'" src="../../assets/common/selected.png" alt="">
+                          </el-dropdown-item>
+                      </el-dropdown-menu>
+                  </el-dropdown>
+                  <div class="line w-1 h-20 bg-c96A0B5 m-r-24" v-if="isLogin"></div>
+                  <div class="flex align-center cursor-point" @click="loginOut" v-if="isLogin">
+                      <img src="../../assets/common/exit.png" alt="">
+                      <div class="text-white m-l-4 m-r-24">{{ $t('common.exit') }}</div>
+                  </div>
+              </div>
             </div>
         </div>
     </div>
